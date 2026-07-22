4dc7d0502417005998aea81a41c2ecdc09eae981
Enzo Lopez <enzolopez@hotmail.com>
2026-07-22 13:23:10 -0400

    Initial Commit

---
+++ Workshop for iOSTests/Workshop_for_iOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  Workshop_for_iOSTests.swift
+//  Workshop for iOSTests
+//
+//  Created by Enzo Lopez on 7/22/26.
+//
+
+import Testing
+@testable import Workshop_for_iOS
+
+struct Workshop_for_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
